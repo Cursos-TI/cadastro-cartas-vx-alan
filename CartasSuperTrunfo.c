2c252fd43937b762f168d7a94640b5008ba477ef
vx-alan <alanricardo021@gmail.com>
2026-04-17 11:13:01 +0000

    Fiz a montagem do Super Trunfo nível novato!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,62 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    int populacao, numero;
+    char estado;
+    char nome [20];
+    char codigo [4];
+    float área, PIB, densidade, per_capita;
+    char carta [5];
   // Área para entrada de dados
+    
+  
+    printf ("Carta: \n");
+    scanf ("%s", carta);
 
+    printf("Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Codigo: \n");
+    scanf("%s", codigo);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", nome);
+
+    printf("População: \n");
+    scanf("%d", &populacao);
+
+    printf("Área(em Km²): \n");
+    scanf("%f", &área);
+
+    printf("PIB: \n");
+    scanf("%e", &PIB);
+
+    printf("Número de Pontos Turísticos: \n");
+    scanf("%i", &numero);
+
+    densidade = (float)(populacao / área );
+    
+    per_capita = (float)(PIB / populacao );
   // Área para exibição dos dados da cidade
 
-return 0;
-} 
+    printf("Carta: %s\n", carta);
+
+    printf("Estado: %c\n", estado);
+
+    printf("Codigo: %s\nNome da cidade: %s\n", codigo, nome);
+
+    printf("População: %d\n", populacao);
+
+    printf("Área(em Km²): %.2f\n", área);
+
+    printf("PIB: %.2f\n", PIB);
+
+    printf("Numero de Ponto Turístico: %i\n", numero);
+
+    printf("Densidade Populacional: %d\n", densidade);
+
+    printf("PIB per Capita: %d\n", per_capita);
+
+    return 0;
+
+  } 
